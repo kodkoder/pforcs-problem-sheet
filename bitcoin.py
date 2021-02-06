741a2c3a3d0c27a8713875e9d9104659e6fdddda
T <tom@Ts-MacBook-Pro.local>
2021-02-06 18:20:59 +0000

    Bitcoin Price Checker

--- bitcoin.py
+++ bitcoin.py
@@ -8,25 +8,22 @@
  
 import requests
 
-# We specify the url from where data will be requested,
-# make a request and store returned data as a dictionary.
+# Request the latest bitcoin data.
 
 url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 returnedData = requests.get(url)
 bitCoinDict = returnedData.json()
 
-# We specfify a name of the key where bitcoin rates are located.
+# We specfify a name of the dictionary key where bitcoin rates are located.
 bitPriceRate = bitCoinDict['bpi']
 
-# Now we find a dictionary/key holding Bitcoin prices in USD Dollars. 
-# We store the price in a variable and print it out to the user.
+# Now we find a dictionary/key holding Bitcoin prices in USD Dollars and print it.
 bitCurrencyUSD = bitPriceRate['USD']
 bitRateUSD = bitCurrencyUSD['rate']
-
 print("\n")
 print("Bitcoin price in USD = {}.".format(bitRateUSD))
 print("\n")
 
-# The same process can be automated for three currencies by using a For Loop. 
+# The same process is automated for three currencies by using a For Loop. 
 for item in bitPriceRate:
     print("Bitcoin price in {} = {}.".format(item,bitPriceRate[item]['rate']))
