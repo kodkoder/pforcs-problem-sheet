476094e65c3a7c90d05530100122a3d29133c6a2
T <tom@Ts-MacBook-Pro.local>
2021-02-08 23:08:58 +0000

    readme

--- bitcoin.py
+++ bitcoin.py
@@ -9,21 +9,17 @@
 import requests
 
 # Request the latest bitcoin data.
-
 url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 returnedData = requests.get(url)
 bitCoinDict = returnedData.json()
 
-# We specfify a name of the dictionary key where bitcoin rates are located.
-bitPriceRate = bitCoinDict['bpi']
-
-# Now we find a dictionary/key holding Bitcoin prices in USD Dollars and print it.
-bitCurrencyUSD = bitPriceRate['USD']
-bitRateUSD = bitCurrencyUSD['rate']
-print("\n")
+# We find a dictionary/item holding Bitcoin price in USD Dollars and print it.
+bitRateUSD = (bitCoinDict["bpi"]["USD"]["rate"])
 print("Bitcoin price in USD = {}.".format(bitRateUSD))
 print("\n")
 
-# The same process is automated for three currencies by using a For Loop. 
+# We repeat the same process for three currencies by using a For Loop. 
+# We specify the nested dictionary holding Bitcoin prices
+bitPriceRate = bitCoinDict['bpi']
 for item in bitPriceRate:
     print("Bitcoin price in {} = {}.".format(item,bitPriceRate[item]['rate']))
