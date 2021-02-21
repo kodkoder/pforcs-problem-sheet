0d9e5366b698854da4c6e8a64ef65d49f202d849
T <tom@Ts-MacBook-Pro.local>
2021-02-21 21:11:45 +0000

    squareroot.py

--- squareroot.py
+++ squareroot.py
@@ -5,6 +5,7 @@
 # 
 # Author: Tomasz
 
+# define a function to calculate square root
 def sq_root(number):
     num = float (number)  # number to get sqare root of
     for i in range (1000):  # iteration number
