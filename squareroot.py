fd6eb0451c4e55a29a6dd4d2317fc788de5852a6
T <tom@Ts-MacBook-Pro.local>
2021-02-21 21:10:49 +0000

    squareroot.py

--- squareroot.py
+++ squareroot.py
@@ -14,4 +14,6 @@ def sq_root(number):
 
 # get the number and transoft it to a float
 num = float(input("Please enter a positive number: "))
+
+# print out the number and the square root formatted to display one decimel only
 print("The square root of {} is approx. {:.1f}.".format(num, sq_root(num)))
