25d117a9b7d23183fa0a5e58e9e65934916184c0
T <tom@Ts-MacBook-Pro.local>
2021-03-02 23:02:23 +0000

    extract_url.py

--- extract-url.py
+++ extract-url.py
@@ -1,15 +1,15 @@
-# collatz.py
+# extract_url.py
 # This program will extract the URLs from an access.log file.
 #
 # Author: Tomasz
 
 import re
 
-filename = "access.log"
+filename = "access.log"  # file we want to analyze
 
+#open file and check each line against the regex
 with open(filename, 'r') as access_logs:
-    count = 0
     for line in access_logs:
         regex = "/[^0-9A-Z/][^w][[a-zA-Z.?=0-9-&/]{1,}"
-        x = re.findall(regex, line)
-        print(x)
+        x = re.findall(regex, line)  # find matches
+        print(x) #print out results
