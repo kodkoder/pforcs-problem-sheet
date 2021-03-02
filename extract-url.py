10293b3ade55d2a45c43e2a1a5dc7a2881cb20d4
T <tom@Ts-MacBook-Pro.local>
2021-03-02 23:03:45 +0000

    extract_url.py

--- extract-url.py
+++ extract-url.py
@@ -7,9 +7,11 @@ import re
 
 filename = "access.log"  # file we want to analyze
 
-#open file and check each line against the regex
+#open file
 with open(filename, 'r') as access_logs:
     for line in access_logs:
         regex = "/[^0-9A-Z/][^w][[a-zA-Z.?=0-9-&/]{1,}"
-        x = re.findall(regex, line)  # find matches
-        print(x) #print out results
+        # find matches
+        x = re.findall(regex, line)
+        # print out results  
+        print(x) 
